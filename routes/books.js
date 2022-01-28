724284be0d72f26b35be9554121ff6376246d496
MirzaMesko <pravi.meskovic.mirza@gmail.com>
2022-01-28 20:24:20 +0100

    add post route to /books endpoint

--- routes/books.js
+++ routes/books.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const BooksController = require("../controllers/BooksController");
-// const ROLES_LIST = require('../config/roles-list');
-// const verifyRoles = require('../middleware/verifyRoles');
+const ROLES_LIST = require('../config/roles-list');
+const verifyRoles = require('../middleware/verifyRoles');
 
 router.route("/").get(BooksController.getAllBooks)
+.post(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Librarian), BooksController.createNewBook)
 
 module.exports = router;
