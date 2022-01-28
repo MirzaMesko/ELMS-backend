6fdb8a313224441257ed11a12fd4cd9b330043e7
MirzaMesko <pravi.meskovic.mirza@gmail.com>
2022-01-28 20:21:08 +0100

    add function for adding new books

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -6,6 +6,27 @@ const getAllBooks = async (req, res) => {
   res.json(books);
 };
 
+const createNewBook = async (req, res) => {
+  
+  if ( !req?.body?.serNo ) {
+       return res.status(400).json({ 'message': 'Serial number is required!'});
+     };
+     const book = await Book.findOne({ serNo: req.body.serNo }).exec();
+      if (book) {
+        return res.status(400).json({ 'message': `A book with this serial number already exists in the database!`});
+      }
+     try {
+
+      const result = await Book.create({ title: req.body.title, 
+        author: req.body.author, year: req.body.year, description: req.body.description, 
+        serNo: req.body.serNo, publisher: req.body.publisher});
+      res.status(201).json(result);
+     } catch (err) {
+       console.log(err);
+     }
+}
+
 module.exports = {
-    getAllBooks
+    getAllBooks, 
+    createNewBook
 }
