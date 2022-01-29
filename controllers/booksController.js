1b42f307acc3b72b7d0aee93436cd84d90e51f69
MirzaMesko <pravi.meskovic.mirza@gmail.com>
2022-01-29 20:53:09 +0100

    add function for updating a book

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -24,9 +24,31 @@ const createNewBook = async (req, res) => {
      } catch (err) {
        console.log(err);
      }
-}
+};
+
+const updateBook = async (req, res) => {
+  if ( !req?.body?.id ) {
+    return res.status(400).json({ 'message': 'Id parameter is required!'});
+  };
+  const book = await Book.findOne({ _id: req.body.id }).exec();
+  if (!book) {
+    return res.status(204).json({ 'message': `No book matches ${req.body.id}.`});
+  }
+
+  if (req?.body?.title) {book.title = req.body.title};
+  if (req?.body?.author) {book.author = req.body.author};
+  if (req?.body?.year) {book.year = req.body.year};
+  if (req?.body?.description) {book.description = req.body.description};
+  if (req?.body?.publisher) {book.publisher = req.body.publisher};
+  if (req?.body?.serNo) {book.serNo = req.body.serNo};
+  
+  const result = await book.save();
+  
+  res.json(result);
+};
 
 module.exports = {
     getAllBooks, 
-    createNewBook
+    createNewBook,
+    updateBook
 }
