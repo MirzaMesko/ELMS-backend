b6dff65101b533b75f0ba302fa0161726d83f5fa
MirzaMesko <pravi.meskovic.mirza@gmail.com>
2022-08-13 12:54:47 +0200

    add catch block to handleEmail()

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -2,30 +2,36 @@
 const nodemailer = require("nodemailer");
 
 const handleEmail = async (req, res) => {
-  let transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      type: "OAuth2",
-      user: process.env.EMAIL,
-      pass: process.env.WORD,
-      clientId: process.env.OAUTH_CLIENTID,
-      clientSecret: process.env.OAUTH_CLIENT_SECRET,
-      refreshToken: process.env.OAUTH_REFRESH_TOKEN,
-    },
-   });
-
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: process.env.EMAIL, // sender address
-    to: req.body.email, // list of receivers
-    subject: req.body.subject, // Subject line
-    text: req.body.text, // plain text body
-    html: `<p>${req.body.text}</p>`, // html body
-  });
-
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-  res.status(200).json(info.messageId);
+  try {
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: process.env.EMAIL,
+        pass: process.env.WORD,
+        clientId: process.env.OAUTH_CLIENTID,
+        clientSecret: process.env.OAUTH_CLIENT_SECRET,
+        refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+      },
+     });
+  
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: process.env.EMAIL, // sender address
+      to: req.body.email, // list of receivers
+      subject: req.body.subject, // Subject line
+      text: req.body.text, // plain text body
+      html: `<p>${req.body.text}</p>`, // html body
+    });
+  
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    res.status(200).json(info.messageId);
+  }
+  catch(error) {
+    return error;
+  }
+  
 }
 
 module.exports = { handleEmail }
