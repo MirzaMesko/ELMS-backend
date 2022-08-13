5c91ea1dc2f904dcb32a674b94f249410e4daaea
MirzaMesko <pravi.meskovic.mirza@gmail.com>
2022-08-13 13:02:15 +0200

    add image to UserInfo in accessToken

--- controllers/refreshTokenController.js
+++ controllers/refreshTokenController.js
@@ -1,43 +1,33 @@
-// const usersDB = {
-//   users: require("../model/users.json"),
-//   setUsers: function (data) {
-//     this.users = data;
-//   },
-// };
-
 const User = require('../model/User');
 
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const handleRefreshToken = async (req, res) => {
-  console.log(req);
   const cookies = req.cookies;
   if (!cookies?.jwt) return res.status(401).json({ message: "test" });
-  console.log(cookies.jwt);
-  const refreshToken = cookies.jwt;
 
-  // const userExists = usersDB.users.find(
-  //   (person) => person.refreshToken === refreshToken
-  // );
-  const userExists = await User.findOne({ refreshToken }).exec();
+  const refreshToken = cookies.jwt;
+  const userExists = await User.findOne({ refreshToken: refreshToken }).exec();
   if (!userExists) return res.sendStatus(401); // Unauthorized
 
   //evaluate jwt
-
   jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
-    if (err || userExists.username !== decoded.username)
+    if (err || userExists.username !== decoded.username) {
       return res.sendStatus(403);
+    }
+      
     const roles = Object.values(userExists.roles);
     const accessToken = jwt.sign(
       {
         'UserInfo': {
-          'username': decoded.username,
+          'username': userExists.username,
           'roles': roles,
+          'image': userExists.image
         },
       },
       process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: "30s" }
+      { expiresIn: "25m" }
     );
     res.json({ accessToken });
   });
